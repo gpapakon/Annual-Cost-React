05c009a45c467d9793f33662239db6874fc268c2
giorgospapakon <papakon1999a@gmail.com>
2021-12-31 13:41:04 +0200

    f

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,11 @@
 import './Card.css';
 
-function Card() {
-
-  return <div className="card"></div>;
+function Card(props) {
+    const classes = 'card ' + props.className;
+    
+  return <div className={classes}>
+      {props.children}
+  </div>;
 }
 
 export default Card;
